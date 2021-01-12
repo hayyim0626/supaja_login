e6a2a5d95a02b755cd18b0844ddf64f7e5f7e4dd
h19960626 <h19960626@gmail.com>
2021-01-12 22:03:02 +0900

    Navbar 레이아웃 구현 완료

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './Navbar.scss'
+import React from "react";
+import "./Navbar.scss";
 
 export default function Navbar() {
-    return (
-        <div>
-            Navbar
+  return (
+    <header className="Navbar">
+      <div className="logo">수파자</div>
+      <div className="groupLecture">
+        <div className='text'>
+          <div>그룹 강의 </div>
+          <div className="live"> Live</div>
         </div>
-    );
+        <div className="line" />
+      </div>
+      <div className="knowHow">공부 노하우</div>
+      <div className="usage">이용방법</div>
+      <div className="signIn">회원가입</div>
+      <div className="login">로그인</div>
+      <div className="freeExperience">무료체험 신청</div>
+    </header>
+  );
 }
