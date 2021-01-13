73869ed2caf97b38e880e7fbc8682eb8809a5416
h19960626 <h19960626@gmail.com>
2021-01-13 17:56:22 +0900

    로그인 컴포넌트 레이아웃 구현 완료

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.scss";
 
 
-
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="Login">
+      <div className="studentLogin">학생 로그인</div>
+      <div className="email">
+        <div className="emailText">이메일 주소</div>
+        <input type="text" className="emailInput" />
+      </div>
+      <div className="password">
+        <div className="passwordText">비밀번호</div>
+        <input type="password" className="passwordInput" />
+      </div>
+      <button className="loginBtn">로그인</button>
+      <div className="signUp">
+        <span className="signUpText">수파자 계정이 없으신가요?</span>
+        <span className="signUpBtn">회원가입</span>
+      </div>
+    </div>
+  );
 }
